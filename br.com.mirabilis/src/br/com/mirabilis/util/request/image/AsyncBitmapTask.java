fbb6b25825f73192665ef68d0356dcb724d0b314
rodrigosimoesrosa <rodrigosimoesrosa@gmail.com>
2013-11-04 14:57:22 -0200

    Http methods

--- br.com.mirabilis/src/br/com/mirabilis/util/request/image/AsyncBitmapTask.java
+++ br.com.mirabilis/src/br/com/mirabilis/util/request/image/AsyncBitmapTask.java
@@ -9,37 +9,53 @@ import br.com.mirabilis.util.image.BufferImage;
 import br.com.mirabilis.util.request.AsyncTaskBase;
 import br.com.mirabilis.util.request.ResponseData;
 import br.com.mirabilis.util.request.http.HttpRequest;
+import br.com.mirabilis.util.request.http.HttpRequest.HttpType;
+import br.com.mirabilis.util.request.http.exception.HttpRequestException;
 import br.com.mirabilis.util.request.listener.DelegateListener;
 
 /**
- * AsyncBitmapTask, tarefa responsável pelo carregamento de uma imagem, utilizando a url como referência e o objeto Delegate.
+ * AsyncBitmapTask, tarefa responsável pelo carregamento de uma imagem,
+ * utilizando a url como referência e o objeto Delegate.
+ * 
  * @author Rodrigo Simões Rosa.
  */
 public class AsyncBitmapTask extends AsyncTaskBase<Void, Void, Bitmap> {
-	
+
 	private String url;
 	private Context context;
 	private Bitmap bmp;
 	private boolean useBuffer;
-	
+
 	/**
 	 * Construtor.
-	 * @param context Necessário para realizar a busca de imagem.
-	 * @param url da imagem que será realizada o download.
-	 * @param delegate objeto que tratará a resposta do download.
+	 * 
+	 * @param context
+	 *            Necessário para realizar a busca de imagem.
+	 * @param url
+	 *            da imagem que será realizada o download.
+	 * @param delegate
+	 *            objeto que tratará a resposta do download.
 	 */
-	public AsyncBitmapTask(Context context, String url, DelegateListener<Bitmap> delegate){
+	public AsyncBitmapTask(Context context, String url,
+			DelegateListener<Bitmap> delegate) {
 		this(context, url, delegate, true);
 	}
-	 
+
 	/**
 	 * Construtor.
-	 * @param context Necessário para realizar a busca de imagem.
-	 * @param url da imagem que será realizada o download.
-	 * @param delegate objeto que tratará a resposta do download.
-	 * @param useBuffer booleano que determinará se aquela imagem será armazenada no buffer.
+	 * 
+	 * @param context
+	 *            Necessário para realizar a busca de imagem.
+	 * @param url
+	 *            da imagem que será realizada o download.
+	 * @param delegate
+	 *            objeto que tratará a resposta do download.
+	 * @param useBuffer
+	 *            booleano que determinará se aquela imagem será armazenada no
+	 *            buffer.
 	 */
-	public AsyncBitmapTask(Context context, String url, DelegateListener<Bitmap> delegate, boolean useBuffer) {
+	public AsyncBitmapTask(Context context, String url,
+			DelegateListener<Bitmap> delegate, boolean useBuffer) {
 		super(delegate);
 		this.url = url;
 		this.context = context;
@@ -48,33 +64,45 @@ public class AsyncBitmapTask extends AsyncTaskBase<Void, Void, Bitmap> {
 
 	@Override
 	protected ResponseData<Bitmap> doInBackground(Void... params) {
-		
+
 		ResponseData<InputStream> response = null;
 		HttpRequest request = null;
-		
-		if(useBuffer){
+
+		if (useBuffer) {
 			bmp = BufferImage.getBitmap(url);
-			if(bmp == null){
-				request = new HttpRequest(context);
-				response = request.get(url);
-				if(response.isSuccessfully()){
-					bmp = BitmapFactory.decodeStream(response.getData());
-					BufferImage.addBitmap(url, bmp);
-					return new ResponseData<Bitmap>(true, response.getMessage(), bmp);
-				}else{
-					return new ResponseData<Bitmap>(false, response.getMessage(), null);
+			if (bmp == null) {
+				try {
+					request = HttpRequest.create(context, HttpType.JAKARTA);
+					response = request.get(url);
+					if (response.isSuccessfully()) {
+						bmp = BitmapFactory.decodeStream(response.getData());
+						BufferImage.addBitmap(url, bmp);
+						return new ResponseData<Bitmap>(true,
+								response.getMessage(), bmp);
+					} else {
+						return new ResponseData<Bitmap>(false,
+								response.getMessage(), null);
+					}
+				} catch (HttpRequestException e) {
+					return new ResponseData<Bitmap>(false, e.getMessage(), null);
 				}
-			}else{
+			} else {
 				return new ResponseData<Bitmap>(true, null, bmp);
 			}
-		}else{
-			request = new HttpRequest(context);
-			response = request.get(url);
-			if(response.isSuccessfully()){
-				bmp = BitmapFactory.decodeStream(response.getData());
-				return new ResponseData<Bitmap>(true, response.getMessage(), bmp);
-			}else{
-				return new ResponseData<Bitmap>(false, response.getMessage(), null);
+		} else {
+			try {
+				request = HttpRequest.create(context, HttpType.JAKARTA);
+				response = request.get(url);
+				if (response.isSuccessfully()) {
+					bmp = BitmapFactory.decodeStream(response.getData());
+					return new ResponseData<Bitmap>(true,
+							response.getMessage(), bmp);
+				} else {
+					return new ResponseData<Bitmap>(false,
+							response.getMessage(), null);
+				}
+			} catch (HttpRequestException e) {
+				return new ResponseData<Bitmap>(false, e.getMessage(), null);
 			}
 		}
 	}
