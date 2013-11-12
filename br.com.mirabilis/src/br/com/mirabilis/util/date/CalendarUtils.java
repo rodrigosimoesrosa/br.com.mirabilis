c6366b318afe3ba2b4738ca46beb3ef6e63ea5c6
rodrigosimoesrosa <rodrigosimoesrosa@gmail.com>
2013-11-12 11:17:18 -0200

    Method's in CalendarUtils

--- br.com.mirabilis/src/br/com/mirabilis/util/date/CalendarUtils.java
+++ br.com.mirabilis/src/br/com/mirabilis/util/date/CalendarUtils.java
@@ -12,13 +12,30 @@ import java.util.Calendar;
 public class CalendarUtils {
 
 	/**
+	 * Pattern default.
+	 */
+	private static final String PATTERN_DEFAULT = "yyyy-MM-dd HH:mm:ss";
+	
+	/**
 	 * Return {@link Calendar} object from {@link String} @param date.
+	 * Use default pattern {@link #PATTERN_DEFAULT}
 	 * @param date
 	 * @return
 	 * @throws ParseException 
 	 */
 	public static Calendar getCalendar(String date) throws ParseException {
-		SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+		return getCalendar(PATTERN_DEFAULT, date);
+	}
+	
+	/**
+	 * Return {@link Calendar} object from {@link String} @param date.
+	 * @param date
+	 * @param pattern Pattern to use in conversion.
+	 * @return
+	 * @throws ParseException 
+	 */
+	public static Calendar getCalendar(String pattern, String date) throws ParseException {
+		SimpleDateFormat sdf = new SimpleDateFormat(pattern);
 		Calendar calendar = Calendar.getInstance();
 		calendar.setTime(sdf.parse(date));
 		return calendar;
@@ -26,11 +43,22 @@ public class CalendarUtils {
 	
 	/**
 	 * Return {@link String} object from {@link Calendar} @param calendar.
+	 * Use default pattern {@link #PATTERN_DEFAULT}
 	 * @param calendar
 	 * @return
 	 */
 	public static String getString(Calendar calendar) {
-		SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+		return getString(PATTERN_DEFAULT, calendar);
+	}
+	
+	/**
+	 * Return {@link String} object from {@link Calendar} @param calendar.
+	 * @param pattern Pattern to use in conversion.
+	 * @param calendar
+	 * @return
+	 */
+	public static String getString(String pattern, Calendar calendar) {
+		SimpleDateFormat sdf = new SimpleDateFormat(pattern);
 		return sdf.format(calendar.getTime());
 	}
 }
